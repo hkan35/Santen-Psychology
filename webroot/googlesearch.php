bc84290ee9d9ff68780d0188996ef471630e5f33
michael <jyi7@student.monash.edu>
2015-09-10 11:02:59 +1000

    9.10
    google search

---
+++ webroot/googlesearch.php
@@ -0,0 +1,27 @@
+<br>
+<br>
+<center><img src="img/favicon.ico" width="60" height="60"></center>
+<center><h1>Santen Psychology<h1></center>				
+				
+				<center><button onclick="goBack()" class="btn btn-success btn-md">Back To Home</button></center>
+
+				<script>
+				function goBack() {
+					window.history.back();
+				}
+				</script>
+			<br><br>
+           
+				<script>
+  (function() {
+    var cx = '010077191880889770901:pbcwqhkurzm';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+        '//cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
+</script>
+<gcse:search></gcse:search>
